b038029a11f9412b492f528002868fe85d96b0f0
pondnarawich <pondnarawich@gmail.com>
2021-06-07 11:54:32 +0700

    Update main.go

--- search/main.go
+++ search/main.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	mgo "gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
-	"strings"
-	"time"
 )
 
 type superhero struct {
@@ -25,29 +23,6 @@ func (this superhero) ToString() string {
 	result = result + fmt.Sprintf("\nAge: %d", this.Age)
 	result = result + fmt.Sprintf("\ndate: %s", this.Superpower)
 	return result
-}
-
-func (this Product) ToString() string {
-	result := fmt.Sprintf("id: %s", this.Id.Hex())
-	result = result + fmt.Sprintf("\nname: %s", this.Name)
-	result = result + fmt.Sprintf("\nprice: %0.1f", this.Price)
-	result = result + fmt.Sprintf("\nquantity: %d", this.Quantity)
-	result = result + fmt.Sprintf("\nstatus: %t", this.Status)
-	result = result + fmt.Sprintf("\ndate: %s", this.Date.Format("2006-01-02"))
-	result = result + fmt.Sprintf("\ncategory id: %s", this.CategoryId.Hex())
-	result = result + fmt.Sprintf("\ncolors: %s", strings.Join(this.Colors, ", "))
-	return result
-}
-
-type Product struct {
-	Id         bson.ObjectId `bson:"_id"`
-	Name       string        `bson:"name"`
-	Price      float64       `bson:"price"`
-	Quantity   int64         `bson:"quantity"`
-	Status     bool          `bson:"status"`
-	Date       time.Time     `bson:"date"`
-	CategoryId bson.ObjectId `bson:"categoryId"`
-	Colors     []string      `bson:"colors"`
 }
 
 func GetMongoDB() (*mgo.Collection, error) {
@@ -88,9 +63,7 @@ func (this hero) FindNameStartsWith(keyword string) ([]superhero, error) {
 
 func main(){
 	var search hero
-
-	fmt.Println("Find the product with the name starting with lap")
-	results, err := search.FindNameStartsWith("S")
+	results, err := search.FindNameStartsWith("z")
 	if err != nil {
 		fmt.Println(err)
 	} else {
