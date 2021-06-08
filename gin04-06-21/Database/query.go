8461a2d80fae9d411e031cc3e18088fb3306ea93
proudkittapa <proudy2013@gmail.com>
2021-06-08 17:07:30 +0700

    -

--- gin04-06-21/Database/query.go
+++ gin04-06-21/Database/query.go
@@ -10,17 +10,6 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type UpdateSuperhero struct {
-	ID         string   `bson:"_id" json:"id" validate:"updateID"`
-	Name       string   `bson:"name" json:"name"`
-	ActualName string   `bson:"actual_name" json:"actual_name" validate:"uniqueActualName2"`
-	Gender     string   `bson:"gender" json:"gender"`
-	BirthDate  int64    `bson:"birth_date" json:"birth_date"`
-	Height     int      `bson:"height" json:"height" validate:"gte=0"`
-	SuperPower []string `bson:"super_power" json:"super_power"`
-	Alive      bool     `bson:"alive" json:"alive"`
-}
-
 //func MaxId() string {
 //	var result bson.M
 //	var currID SuperheroQ
--- gin04-06-21/Database/search.go
+++ gin04-06-21/Database/search.go
@@ -10,21 +10,6 @@ import (
 	// "touch/Database"
 )
 
-type SuperheroQ struct {
-	ID         string   `bson:"_id" json:"id"`
-	Name       string   `bson:"name" json:"name" validate:"required,uniqueName"`
-	ActualName string   `bson:"actual_name" json:"actual_name" validate:"required,uniqueActualName"`
-	Gender     string   `bson:"gender" json:"gender"`
-	BirthDate  int64    `bson:"birth_date" json:"birth_date"`
-	Height     int      `bson:"height" json:"height" validate:"gte=0"`
-	SuperPower []string `bson:"super_power" json:"super_power"`
-	Alive      bool     `bson:"alive" json:"alive"`
-}
-
-type SearchValue struct {
-	Value string `bson:"value"`
-}
-
 func SearchName(keyword string) []SuperheroQ {
 	fmt.Println("Searching", keyword)
 	var result []SuperheroQ
@@ -113,11 +98,11 @@ func Search(keyword string) []SuperheroQ {
 	fmt.Println("Searching", keyword)
 	var result []SuperheroQ
 	cursor, err := Coll.Find(Ctx,
-	bson.M{
-		"$or": bson.A{
-			bson.M{"name": primitive.Regex{Pattern: keyword, Options: "i"},},
-			bson.M{"actual_name": primitive.Regex{Pattern: keyword, Options: "i"},},
-		}})
+		bson.M{
+			"$or": bson.A{
+				bson.M{"name": primitive.Regex{Pattern: keyword, Options: "i"}},
+				bson.M{"actual_name": primitive.Regex{Pattern: keyword, Options: "i"}},
+			}})
 	if err != nil {
 		log.Fatal(err)
 	}
