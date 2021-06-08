41b4cc22603059376dde5427eb7f8be18fbdd69c
proudkittapa <proudy2013@gmail.com>
2021-06-08 12:18:12 +0700

    -

--- gin04-06-21/main.go
+++ gin04-06-21/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"time"
 	"touch/Database"
 
 	"github.com/gin-gonic/gin"
@@ -48,14 +47,14 @@ func setupRouter() *gin.Engine {
 }
 
 func insert(c *gin.Context) {
-	var t Database.SuperheroQ
-	if err := c.ShouldBindJSON(&hero); err != nil{
+	var hero Database.SuperheroQ
+	if err := c.ShouldBindJSON(&hero); err != nil {
 		c.JSON(http.StatusBadRequest, "can't bind")
 		return
 	}
-	
-	val, message := validate(hero)
-	if !val{
+
+	val, message := validateHero(hero)
+	if !val {
 		c.JSON(http.StatusBadRequest, message)
 		return
 	}
@@ -65,64 +64,49 @@ func insert(c *gin.Context) {
 
 func updateId(c *gin.Context) {
 	id := c.Param("id")
-	// i, err := strconv.Atoi(id)
-	// if err != nil {
-	// 	c.JSON(http.StatusNotFound, "wrong format should be int not string")
+
+	// if !Database.CheckExistID(id) {
+	// 	c.JSON(http.StatusNotFound, "this id doesn't exist")
 	// 	return
 	// }
-	if !Database.CheckExistID(id) {
-		c.JSON(http.StatusNotFound, "this id doesn't exist")
+	var hero Database.SuperheroQ
+	if err := c.ShouldBindJSON(&hero); err != nil {
+		c.JSON(http.StatusBadRequest, "can't bind")
 		return
 	}
-	buf := make([]byte, 1024)
-	num, _ := c.Request.Body.Read(buf)
-	reqBody := string(buf[0:num])
-	var hero Database.SuperheroQ
-	err = json.Unmarshal(buf[0:num], &t)
-	if err != nil {
-		panic(err)
-	}
-	// hero.ID = i
 
-	// if t.Age < 0 {
-	// 	c.JSON(http.StatusNotFound, "age is less than 0")
-	// 	return
-	// }
-	Database.Update(hero, ID)
-	c.JSON(http.StatusOK, reqBody)
+	val, message := validateHero(hero)
+	if !val {
+		c.JSON(http.StatusBadRequest, message)
+		return
+	}
+
+	Database.Update(hero, id)
+	c.JSON(http.StatusOK, message)
 }
 
 func deleteId(c *gin.Context) {
 	id := c.Param("id")
-	i, err := strconv.Atoi(id)
-	if err != nil {
-		c.JSON(http.StatusNotFound, "wrong format should be int not string")
-		return
-	}
-	if !Database.CheckExistID(i) {
+	if !Database.CheckExistID(id) {
 		// fmt.Println("this id doesn't exist")
 		c.JSON(http.StatusNotFound, "this id doesn't exist")
 		return
 	}
-	Database.Delete(i)
-	c.JSON(http.StatusOK, i)
+	Database.Delete(id)
+	c.JSON(http.StatusOK, "deleted")
 }
 
 func viewId(c *gin.Context) {
 	id := c.Param("id")
-	i, err := strconv.Atoi(id)
-	if err != nil {
-		c.JSON(http.StatusNotFound, "wrong format should be int not string")
-		return
-	}
-	if !Database.CheckExistID(i) {
+
+	if !Database.CheckExistID(id) {
 		// fmt.Println("this id doesn't exist")
 		c.JSON(http.StatusNotFound, "this id doesn't exist")
 		return
 	}
 
-	var a Database.SuperheroQ = Database.View(i) //return struct
-	c.JSON(http.StatusOK, a)
+	var data Database.SuperheroQ = Database.View(id)
+	c.JSON(http.StatusOK, data)
 }
 
 func viewAll(c *gin.Context) {
@@ -165,7 +149,7 @@ func search(c *gin.Context) {
 	if err != nil {
 		fmt.Println(err)
 	}
-	data := Database.Search(v.Value)
+	data := Database.SearchName(v.Value)
 	if data == nil {
 		c.JSON(http.StatusOK, "No result")
 		return
@@ -173,30 +157,9 @@ func search(c *gin.Context) {
 	c.JSON(http.StatusOK, data)
 }
 
-// func checkDate(name string) bool {
-// 	myDateString := name
-// 	fmt.Println("My Starting Date:\t", myDateString)
-// 	fmt.Printf("%T\n", myDateString)
-
-// 	// Parse the date string into Go's time object
-// 	// The 1st param specifies the format, 2nd is our date string
-// 	myDate, err := time.Parse("2006-01-02", myDateString)
-// 	fmt.Printf("%T\n", myDateString)
-// 	if err != nil {
-// 		return false
-// 	}
-// 	// Format uses the same formatting style as parse, or we can use a pre-made constant
-// 	fmt.Println("My Date Reformatted:\t", myDate.Format(time.RFC822))
-// 	// fmt.Printf("%T\n", myDateString)
-// 	// In Y-m-d
-// 	fmt.Println("Just The Date:\t\t", myDate.Format("2006-01-02"))
-// 	// fmt.Printf("%T\n", myDateString)
-// 	return true
-// }
-
-func validate(hero Database.SuperheroQ) bool, string{
+func validateHero(hero Database.SuperheroQ) (bool, string) {
 	err := validate.Struct(hero)
-	message:= ""
+	message := ""
 	if err != nil {
 		for _, err := range err.(validator.ValidationErrors) {
 
@@ -212,7 +175,7 @@ func validate(hero Database.SuperheroQ) bool, string{
 			fmt.Println("Param:", err.Param())
 			message = message + err.Namespace() + err.Field() + err.StructNamespace() + err.StructField() + err.Tag() + err.ActualTag() + err.Kind() + err.Type() + err.Value() + err.Param()
 			fmt.Println()
-			
+
 		}
 		return false, message
 	}
