ecb90350dcfe011ccdf020ebff131e0b104785de
proudkittapa <proudy2013@gmail.com>
2021-06-08 14:22:25 +0700

    -

--- gin04-06-21/Database/search.go
+++ gin04-06-21/Database/search.go
@@ -13,7 +13,7 @@ import (
 type SuperheroQ struct {
 	ID         string   `bson:"_id" json:"id"`
 	Name       string   `bson:"name" json:"name" validate:"required,uniqueName"`
-	ActualName string   `bson:"actual_name" json:"actual_name" validate:"uniqueActualName"`
+	ActualName string   `bson:"actual_name" json:"actual_name" validate:"required,uniqueActualName"`
 	Gender     string   `bson:"gender" json:"gender"`
 	BirthDate  int64    `bson:"birth_date" json:"birth_date"`
 	Height     int      `bson:"height" json:"height" validate:"gte=0"`
