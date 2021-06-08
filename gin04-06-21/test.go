009f8ee9c4307008f6ac425ad9e45e37945219a8
pondnarawich <pondnarawich@gmail.com>
2021-06-08 21:06:46 +0700

    add comment to test drive

--- gin04-06-21/test.go
+++ gin04-06-21/test.go
@@ -5,20 +5,32 @@ import
 )
 
 func init(){
-
+	//connect http tp application
 }
 
 func main(){
-
+	//call other test function and print out the result
 }
 
 func Search(searchInput string) string{
-
+	//send http to search function and input the "searchInput" and return the search output as string
 }
+
 func TestSearchWithNoCommon(t *testing.T){
 	searchInput := "asdasd"
 	searchOutput := Search(searchInput)
-	if searchInput != searchOutput{
-		t.Error("Searching for ",searchInput," didn't get the result of ",searchOutput)
+	ExpectedResult := "No result"
+	if searchIOutput != ExpectedResult{
+		t.Error("Searching for ",searchInput," for no common didn't get the result of ",searchOutput)
 	}
 }
+
+func TestSearchWithNoCommon(t *testing.T){
+	searchInput := "su"
+	searchOutput := Search(searchInput)
+	ExpectedResult := ""
+	if searchIOutput != ExpectedResult{
+		t.Error("Searching for ",searchInput," for common didn't get the result of ",searchOutput)
+	}
+}
+
