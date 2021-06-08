a37ef86e4b9cd6e5044e3a49e9705f7045e78805
gnnchya <75212443+gnnchya@users.noreply.github.com>
2021-06-08 22:51:30 +0700

    unit test tiny change

--- gin04-06-21/main_test.go
+++ gin04-06-21/main_test.go
@@ -16,7 +16,7 @@ func TestSearchWithNoCommon(t *testing.T){
 	searchInput := "asdasd"
 	searchOutput := Search(searchInput)
 	ExpectedResult := "No result"
-	if searchIOutput != ExpectedResult{
+	if searchOutput != ExpectedResult{
 		t.Error("Searching for ",searchInput," for no common alphabet/phrase didn't get the result of ",searchOutput)
 	}
 }
@@ -25,7 +25,7 @@ func TestSearchWithCommon(t *testing.T){//
 	searchInput := "su"
 	searchOutput := Search(searchInput)
 	ExpectedResult := ""
-	if searchIOutput != ExpectedResult{
+	if searchOutput != ExpectedResult{
 		t.Error("Searching for ",searchInput," for common alphabet/phrase didn't get the result of ",searchOutput)
 	}
 }
