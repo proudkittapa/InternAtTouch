f303d1760f174b2505c24011e423f29a833a0ddb
pondnarawich <pondnarawich@gmail.com>
2021-06-09 16:45:24 +0700

    add return error

--- newApp/repository/mongodb/search.go
+++ newApp/repository/mongodb/search.go
@@ -7,35 +7,34 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
-	"log"
 )
 
-func addToArray(cursor *mongo.Cursor,err error,ctx context.Context) []domain.InsertQ{
+func addToArray(cursor *mongo.Cursor,err error,ctx context.Context) ([]domain.InsertQ, error) {
 	var result []domain.InsertQ
 	for cursor.Next(ctx) {
 		var resultBson bson.M
 		var resultStruct domain.InsertQ
 		if err = cursor.Decode(&resultBson); err != nil {
-			log.Fatal(err)
+			return result,err
 		}
 		bsonBytes, _ := bson.Marshal(resultBson)
 		bson.Unmarshal(bsonBytes, &resultStruct)
 		fmt.Println(resultStruct)
 		result = append(result, resultStruct)
 	}
-	return result
+	return result,err
 }
 
-func (repo *Repository)SearchDefault(ctx context.Context,field string, keyword string) []domain.InsertQ{
+func (repo *Repository)SearchDefault(ctx context.Context,field string, keyword string) ([]domain.InsertQ,error){
 	fmt.Println("Searching for ",keyword,"in",field)
 	cursor, err := repo.Coll.Find(ctx, bson.M{field: primitive.Regex{Pattern: keyword, Options: "i"}})
 	if err != nil {
-		log.Fatal(err)
+		return addToArray(cursor,err,ctx)
 	}
 	return addToArray(cursor,err,ctx)
 }
 
-func (repo *Repository)SearchByBothName(ctx context.Context,field string,keyword string) []domain.InsertQ {
+func (repo *Repository)SearchByBothName(ctx context.Context,field string,keyword string) ([]domain.InsertQ, error) {
 	fmt.Println("Searching for ",keyword,"in",field)
 	cursor, err := repo.Coll.Find(ctx,
 		bson.M{
@@ -44,7 +43,7 @@ func (repo *Repository)SearchByBothName(ctx context.Context,field string,keyword
 				bson.M{"actual_name": primitive.Regex{Pattern: keyword, Options: "i"}},
 			}})
 	if err != nil {
-		log.Fatal(err)
+		addToArray(cursor,err,ctx)
 	}
 	return addToArray(cursor,err,ctx)
 }
