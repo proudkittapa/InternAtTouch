f795b3a891f5d7512187b0a80322a49ade0ad525
pondnarawich <pondnarawich@gmail.com>
2021-06-09 17:57:47 +0700

    gunn fuck git up

--- newApp/service/user/implement/search.go
+++ newApp/service/user/implement/search.go
@@ -2,7 +2,6 @@ package implement
 
 import (
 	"context"
-
 	"github.com/gnnchya/InternAtTouch/tree/Develop-optimized/newApp/service/user/userin"
 	// "github.com/touchtechnologies-product/go-blueprint-clean-architecture/service/util"
 	// "github.com/touchtechnologies-product/go-blueprint-clean-architecture/service/company/companyin"
@@ -11,13 +10,5 @@ import (
 
 func (impl *implementation) Search(ctx context.Context, input *userin.CreateInput) (ID string, err error) {
 
-//	switch field{
-//	case "name", "actual_name", "gender", "super_power":
-//SearchByField()
-//	case "both_name":
-//SearchByBothName
-//	default:
-//		return
-//	}
 	return user.Name, nil
 }
