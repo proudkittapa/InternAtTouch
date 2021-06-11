957aca9fbb203ea60e91938d9eedf5092142caa3
proudkittapa <proudy2013@gmail.com>
2021-06-11 12:18:42 +0700

    -

--- superHero/newApp/service/user/userin/create.go
+++ superHero/newApp/service/user/userin/create.go
@@ -13,19 +13,14 @@ import (
 // } // @Name StaffCreateInput
 
 type CreateInput struct {
-	ID           string   `json:"id"`
-	Name         string   `son:"name" validate:"required"`
-	ActualName   string   `json:"actual_name" validate:"required"`
-	Gender       string   `json:"gender"`
-	BirthDate    int64    `json:"birth_date"`
-	Height       int      `json:"height" validate:"gte=0"`
-	SuperPower   []string `json:"super_power"`
-	Alive        bool     `json:"alive"`
-	Universe     string   `json:"universe"`
-	Movies       []string `json:"movies"`
-	Enemies      []string `json:"enemies"`
-	FamilyMember []string `json:"family_member"`
-	About        string   `json:"about"`
+	ID         string   `json:"id"`
+	Name       string   `son:"name" validate:"required"`
+	ActualName string   `json:"actual_name" validate:"required"`
+	Gender     string   `json:"gender"`
+	BirthDate  int64    `json:"birth_date"`
+	Height     int      `json:"height" validate:"gte=0"`
+	SuperPower []string `json:"super_power"`
+	Alive      bool     `json:"alive"`
 }
 
 func MakeTestCreateInput() (input *CreateInput) {
@@ -37,18 +32,13 @@ func MakeTestCreateInput() (input *CreateInput) {
 
 func CreateInputToUserDomain(input *CreateInput) (user *domain.InsertQ) {
 	return &domain.InsertQ{
-		ID:           input.ID,
-		Name:         input.Name,
-		ActualName:   input.ActualName,
-		Gender:       input.Gender,
-		BirthDate:    input.BirthDate,
-		Height:       input.Height,
-		SuperPower:   input.SuperPower,
-		Alive:        input.Alive,
-		Universe:     input.Universe,
-		Movies:       input.Movies,
-		Enemies:      input.Enemies,
-		FamilyMember: input.FamilyMember,
-		About:        input.About,
+		ID:         input.ID,
+		Name:       input.Name,
+		ActualName: input.ActualName,
+		Gender:     input.Gender,
+		BirthDate:  input.BirthDate,
+		Height:     input.Height,
+		SuperPower: input.SuperPower,
+		Alive:      input.Alive,
 	}
 }
