949d44ca5b7dde6fdfffa68e76b633a0c6d5ae57
pondnarawich <pondnarawich@gmail.com>
2021-06-11 10:20:11 +0700

    -

--- superHero/newApp/service/user/userin/update.go
+++ superHero/newApp/service/user/userin/update.go
@@ -6,14 +6,19 @@ import (
 )
 
 type UpdateInput struct {
-	ID         string   `json:"id"`
-	Name       string   `json:"name" validate:"required"`
-	ActualName string   `json:"actual_name" validate:"required"`
-	Gender     string   `json:"gender"`
-	BirthDate  int64    `json:"birth_date"`
-	Height     int      `json:"height" validate:"gte=0"`
-	SuperPower []string `json:"super_power"`
-	Alive      bool     `json:"alive"`
+	ID           string   `json:"id"`
+	Name         string   `json:"name" validate:"required"`
+	ActualName   string   `json:"actual_name" validate:"required"`
+	Gender       string   `json:"gender"`
+	BirthDate    int64    `json:"birth_date"`
+	Height       int      `json:"height" validate:"gte=0"`
+	SuperPower   []string `json:"super_power"`
+	Alive        bool     `json:"alive"`
+	Universe     string   `json:"universe"`
+	Movies       []string `json:"movies"`
+	Enemies      []string `json:"enemies"`
+	FamilyMember []string `json:"family_member"`
+	About        string   `json:"about"`
 } // @Name StaffCreateInput
 
 func MakeTestUpdateInput() (input *UpdateInput) {
@@ -27,13 +32,18 @@ func MakeTestUpdateInput() (input *UpdateInput) {
 
 func UpdateInputToUserDomain(input *UpdateInput) (user *domain.UpdateQ) {
 	return &domain.UpdateQ{
-		ID:         input.ID,
-		Name:       input.Name,
-		ActualName: input.ActualName,
-		Gender:     input.Gender,
-		BirthDate:  input.BirthDate,
-		Height:     input.Height,
-		SuperPower: input.SuperPower,
-		Alive:      input.Alive,
+		ID:           input.ID,
+		Name:         input.Name,
+		ActualName:   input.ActualName,
+		Gender:       input.Gender,
+		BirthDate:    input.BirthDate,
+		Height:       input.Height,
+		SuperPower:   input.SuperPower,
+		Alive:        input.Alive,
+		Universe:     input.Universe,
+		Movies:       input.Movies,
+		Enemies:      input.Enemies,
+		FamilyMember: input.FamilyMember,
+		About:        input.About,
 	}
 }
