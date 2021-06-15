99f20c0ad6ae8e3f19b9d30a4031b79d62d35d14
proudkittapa <proudy2013@gmail.com>
2021-06-15 17:20:13 +0700

    kafka in repo

--- superHero/newApp2/repository/kafka/consumer.go
+++ superHero/newApp2/repository/kafka/consumer.go
@@ -8,7 +8,7 @@ import (
 
 func (message Kafka) Consumer() {
 	go message.Broker.Start(func(ctx context.Context, err error) {
-		time.Sleep(2 * time.Second)
+		time.Sleep(40 * time.Second)
 		os.Exit(0)
 	})
 	return
--- superHero/newApp2/repository/kafka/handler.go
+++ superHero/newApp2/repository/kafka/handler.go
@@ -3,8 +3,6 @@ package kafka
 import (
 	"github.com/gnnchya/InternAtTouch/tree/Develop-optimized/newApp/service/msgbroker/msgbrokerin"
 	"github.com/touchtechnologies-product/message-broker/common"
-
-	//"git.touchdevops.com/touchtechnologies/authentication-service/service/msgbroker/msgbrokerin"
 )
 
 func (message Kafka) RegisterHandler(topic msgbrokerin.TopicMsgBroker, handler common.Handler) {
