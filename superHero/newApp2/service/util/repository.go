e9b9b90938ad63451cae1eb766ff1618ba227f1a
gnnchya <75212443+gnnchya@users.noreply.github.com>
2021-06-16 09:10:56 +0700

    Update repository.go

--- superHero/newApp2/service/util/repository.go
+++ superHero/newApp2/service/util/repository.go
@@ -2,22 +2,22 @@ package util
 
 import (
 	"context"
+	"github.com/gnnchya/InternAtTouch/tree/Develop-optimized/newApp/domain"
 	"github.com/gnnchya/InternAtTouch/tree/Develop-optimized/newApp/service/msgbroker/msgbrokerin"
 	"github.com/touchtechnologies-product/message-broker/common"
-
-	"github.com/gnnchya/InternAtTouch/tree/Develop-optimized/newApp/domain"
 )
 
 type Repository interface {
-	Create(ctx context.Context, ent interface{}) (err error)
-	Update(ctx context.Context, ent interface{}, ID string) (err error)
-	Delete(ctx context.Context, id string) (err error)
-	Search(ctx context.Context, s *domain.SearchValue) (result string, err error)
-	View(ctx context.Context, id string) (a domain.InsertQ, err error)
-	ViewAll(ctx context.Context, perPage int, page int) (a []domain.InsertQ, err error)
-	CheckExistName(ctx context.Context, name string) (bool, error)
-	CheckExistActualName(ctx context.Context, actualName string) (bool, error)
+	Insert(ctx context.Context, title domain.UpdateStruct) error
+	Update(ctx context.Context, title domain.UpdateStruct) error
+	Delete(ctx context.Context, id string) error
 }
+//	Search(ctx context.Context, s *domain.SearchValue) (result string, err error)
+//	View(ctx context.Context, id string) (a domain.InsertQ, err error)
+//	ViewAll(ctx context.Context, perPage int, page int) (a []domain.InsertQ, err error)
+//	CheckExistName(ctx context.Context, name string) (bool, error)
+//	CheckExistActualName(ctx context.Context, actualName string) (bool, error)
+//}
 
 type RepositoryUsers interface{
 	Repository
