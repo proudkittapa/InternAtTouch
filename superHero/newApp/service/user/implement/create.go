d9a83b54fbd8c436310cdf902623bc01a6c62579
proudkittapa <proudy2013@gmail.com>
2021-06-16 10:59:11 +0700

    -

--- superHero/newApp/service/user/implement/create.go
+++ superHero/newApp/service/user/implement/create.go
@@ -3,29 +3,25 @@ package implement
 import (
 	"context"
 	"fmt"
-	"log"
-
 	"github.com/gnnchya/InternAtTouch/tree/Develop-optimized/newApp/service/msgbroker/msgbrokerin"
 	"github.com/gnnchya/InternAtTouch/tree/Develop-optimized/newApp/service/user/userin"
 	"github.com/modern-go/reflect2"
-	// "github.com/touchtechnologies-product/go-blueprint-clean-architecture/service/util"
-	// "github.com/touchtechnologies-product/go-blueprint-clean	-architecture/service/company/companyin"
-	// "github.com/touchtechnologies-product/go-blueprint-clean-architecture/service/util"
+	"log"
 )
 
 func (impl *implementation) Create(ctx context.Context, input *userin.CreateInput) (ID string, err error) {
-	defer func() {
-		if !reflect2.IsNil(err) {
+	//var msg []byte
+	defer func(){
+		if !reflect2.IsNil(err){
 			return
 		}
-		if err == impl.sendMsgCreate(input) {
+		if err == impl.sendMsgCreate(input){
 			log.Println(err)
 		}
-
 	}()
 	err = impl.validator.Validate(input)
 	if err != nil {
-		fmt.Println("validte", err)
+		fmt.Println("validate", err)
 		return "validate error", err
 	}
 
@@ -44,15 +40,21 @@ func (impl *implementation) Create(ctx context.Context, input *userin.CreateInpu
 }
 
 func (impl *implementation) sendMsgCreate(input *userin.CreateInput) (err error) {
-	return impl.MsgSender(msgbrokerin.TopicUser, userin.MsgBrokerCreate{
+	return impl.MsgSender("create", userin.MsgBrokerCreate{
 		Action:     msgbrokerin.ActionCreate,
-		ID:         input.ID,
-		Name:       input.Name,
-		ActualName: input.ActualName,
-		Gender:     input.Gender,
-		BirthDate:  input.BirthDate,
-		Height:     input.Height,
-		SuperPower: input.SuperPower,
-		Alive:      input.Alive,
+		ID:             input.ID,
+		Name:           input.Name,
+		ActualName:     input.ActualName,
+		ActualLastName: input.ActualLastName,
+		Gender:         input.Gender,
+		BirthDate:      input.BirthDate,
+		Height:         input.Height,
+		SuperPower:     input.SuperPower,
+		Alive:          input.Alive,
+		Universe:       input.Universe,
+		Movies:         input.Movies,
+		Enemies:        input.Enemies,
+		FamilyMember:   input.FamilyMember,
+		About:          input.About,
 	})
 }
--- superHero/newApp/service/user/implement/update.go
+++ superHero/newApp/service/user/implement/update.go
@@ -3,14 +3,25 @@ package implement
 import (
 	"context"
 	"fmt"
-
+	"github.com/gnnchya/InternAtTouch/tree/Develop-optimized/newApp/service/msgbroker/msgbrokerin"
 	"github.com/gnnchya/InternAtTouch/tree/Develop-optimized/newApp/service/user/userin"
+	"github.com/modern-go/reflect2"
+	"log"
+
 	// "github.com/touchtechnologies-product/go-blueprint-clean-architecture/service/util"
 	// "github.com/touchtechnologies-product/go-blueprint-clean	-architecture/service/company/companyin"
 	// "github.com/touchtechnologies-product/go-blueprint-clean-architecture/service/util"
 )
 
 func (impl *implementation) Update(ctx context.Context, input *userin.UpdateInput) (ID string, err error) {
+	defer func(){
+		if !reflect2.IsNil(err){
+			return
+		}
+		if err == impl.sendMsgUpdate(input){
+			log.Println(err)
+		}
+	}()
 	err = impl.validator.Validate(input)
 	if err != nil {
 		fmt.Println("validte", err)
@@ -19,11 +30,31 @@ func (impl *implementation) Update(ctx context.Context, input *userin.UpdateInpu
 
 	user := userin.UpdateInputToUserDomain(input)
 
-	err = impl.repo.Update(ctx, user, user.ID)
+	//err = impl.repo.Update(ctx, user, user.ID)
 	if err != nil {
 		// fmt.Println("er")
 		return "", err
 	}
 
 	return user.Name, nil
+}
+
+func (impl *implementation) sendMsgUpdate(input *userin.UpdateInput) (err error) {
+	return impl.MsgSender("update", userin.MsgBrokerCreate{
+		Action:     msgbrokerin.ActionUpdate,
+		ID:             input.ID,
+		Name:           input.Name,
+		ActualName:     input.ActualName,
+		ActualLastName: input.ActualLastName,
+		Gender:         input.Gender,
+		BirthDate:      input.BirthDate,
+		Height:         input.Height,
+		SuperPower:     input.SuperPower,
+		Alive:          input.Alive,
+		Universe:       input.Universe,
+		Movies:         input.Movies,
+		Enemies:        input.Enemies,
+		FamilyMember:   input.FamilyMember,
+		About:          input.About,
+	})
 }
