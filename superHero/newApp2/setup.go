e890bcf4e16acc1a4ef75f4d1485d7658563d48d
gnnchya <75212443+gnnchya@users.noreply.github.com>
2021-06-16 09:54:21 +0700

    update

--- superHero/newApp2/setup.go
+++ superHero/newApp2/setup.go
@@ -1,34 +1,47 @@
 package main
 
 import (
-	"context"
 	"github.com/gnnchya/InternAtTouch/tree/Develop-optimized/newApp/app"
 	"github.com/gnnchya/InternAtTouch/tree/Develop-optimized/newApp/config"
 	"github.com/gnnchya/InternAtTouch/tree/Develop-optimized/newApp/repository/kafka"
-	//userRepo "github.com/gnnchya/InternAtTouch/tree/Develop-optimized/newApp/repository/user"
-	msgBrokerService "github.com/gnnchya/InternAtTouch/tree/Develop-optimized/newApp/service/msgbroker/implement"
+
 	"github.com/gnnchya/InternAtTouch/tree/Develop-optimized/newApp/service/msgbroker/msgbrokerin"
-	userService "github.com/gnnchya/InternAtTouch/tree/Develop-optimized/newApp/service/user/implement"
-	validatorService "github.com/gnnchya/InternAtTouch/tree/Develop-optimized/newApp/service/validator"
 	"log"
 )
 
-func newApp(appConfig *config.Config) *app.App {
-	ctx := context.Background()
+//func newApp(appConfig *config.Config) *app.App {
+//	ctx := context.Background()
+//	//uRepo, err := userRepo.New(ctx, appConfig.MongoDBEndpoint, appConfig.MongoDBName, appConfig.MongoDBHeroTableName)
+//	//panicIfErr(err)
+//	kRepo, err := kafka.New(configKafka(appConfig))
+//	panicIfErr(err)
+//	//validator := validatorService.New(uRepo)
+//
+//	//user := userService.New(validator, uRepo, kRepo)
+//	//msgService := msgBrokerService.New(kRepo, user)
+//	//wg.Add(1)
+//	//msgService.Receiver(topics)
+//	//time.Sleep(10 * time.Second)
+//	//return app.New(user)
+//}
+
+func newApp(appConfig *config.Config) (a *app.App) {
+	//ctx := context.Background()
 	//uRepo, err := userRepo.New(ctx, appConfig.MongoDBEndpoint, appConfig.MongoDBName, appConfig.MongoDBHeroTableName)
-	panicIfErr(err)
-	kRepo, err := kafka.New(configKafka(appConfig))
-	panicIfErr(err)
+	//panicIfErr(err)
+	//kRepo, err := kafka.New(configKafka(appConfig))
+	//panicIfErr(err)
 	//validator := validatorService.New(uRepo)
 
 	//user := userService.New(validator, uRepo, kRepo)
-	msgService := msgBrokerService.New(kRepo, user)
+	//msgService := msgBrokerService.New(kRepo, user)
 	//wg.Add(1)
-	msgService.Receiver(topics)
+	//msgService.Receiver(topics)
 	//time.Sleep(10 * time.Second)
-
-	return app.New(user)
+	//return app.New(user)
+	return a
 }
+
 
 
 func panicIfErr(err error) {
