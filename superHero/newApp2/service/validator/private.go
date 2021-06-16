8bff8ab0818fab2e17b9daa6bbba88e420e0144e
gnnchya <75212443+gnnchya@users.noreply.github.com>
2021-06-16 09:44:19 +0700

    Update private.go

--- superHero/newApp2/service/validator/private.go
+++ superHero/newApp2/service/validator/private.go
@@ -27,48 +27,48 @@ func (v *GoPlayGroundValidator) checkTH(structLV validator.StructLevel, name str
 }
 
 func (v *GoPlayGroundValidator) checkUserNameUnique(ctx context.Context, structLV validator.StructLevel, name string) (user *domain.InsertQ) {
-	a, err := v.userRepo.CheckExistName(ctx, name)
-	if err != nil {
-		structLV.ReportError(err, "err validation", "err validation", "error from database", "")
-	}
-	if a == true {
-		structLV.ReportError(name, "name", "name", "unique", "")
-	}
+	//a, err := v.userRepo.CheckExistName(ctx, name)
+	//if err != nil {
+	//	structLV.ReportError(err, "err validation", "err validation", "error from database", "")
+	//}
+	//if a == true {
+	//	structLV.ReportError(name, "name", "name", "unique", "")
+	//}
 	return user
 }
 
 func (v *GoPlayGroundValidator) checkUserActualNameUnique(ctx context.Context, structLV validator.StructLevel, name string) (user *domain.InsertQ) {
-	a, _ := v.userRepo.CheckExistActualName(ctx, name)
-	// if err != nil {
-	// 	structLV.ReportError(err, "err validation", "err validation", "error from database", "")
-	// }
-	if a == true {
-		structLV.ReportError(name, "actual_name", "actual_name", "unique", "")
-	}
+	//a, _ := v.userRepo.CheckExistActualName(ctx, name)
+	//// if err != nil {
+	//// 	structLV.ReportError(err, "err validation", "err validation", "error from database", "")
+	//// }
+	//if a == true {
+	//	structLV.ReportError(name, "actual_name", "actual_name", "unique", "")
+	//}
 	return user
 }
 
 func (v *GoPlayGroundValidator) checkUserNameUniqueUpdate(ctx context.Context, structLV validator.StructLevel, name string, actualName string, id string) (user *domain.UpdateQ) {
-	n, err := v.userRepo.CheckExistName(ctx, name)
-	log.Println("qq", err)
-	an, _ := v.userRepo.CheckExistActualName(ctx, actualName)
-	log.Println("qq1")
-	if n == true { //jer name
-		if an == true { //
-			temp, _ := v.userRepo.View(ctx, id)
-			if temp.Name != name {
-				structLV.ReportError(actualName, "actual_name", "actual_name", "unique", "")
-			}
-		}
-	}
-	if an == true { //jer name
-		if n == true { //
-			temp, _ := v.userRepo.View(ctx, id)
-			if temp.ActualName != actualName {
-				structLV.ReportError(name, "name", "name", "unique", "")
-			}
-		}
-	}
+	//n, err := v.userRepo.CheckExistName(ctx, name)
+	//log.Println("qq", err)
+	//an, _ := v.userRepo.CheckExistActualName(ctx, actualName)
+	//log.Println("qq1")
+	//if n == true { //jer name
+	//	if an == true { //
+	//		temp, _ := v.userRepo.View(ctx, id)
+	//		if temp.Name != name {
+	//			structLV.ReportError(actualName, "actual_name", "actual_name", "unique", "")
+	//		}
+	//	}
+	//}
+	//if an == true { //jer name
+	//	if n == true { //
+	//		temp, _ := v.userRepo.View(ctx, id)
+	//		if temp.ActualName != actualName {
+	//			structLV.ReportError(name, "name", "name", "unique", "")
+	//		}
+	//	}
+	//}
 	return user
 }
 
