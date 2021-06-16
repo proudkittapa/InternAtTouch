2cd9090e8a796c1cf7641123720ede70be5cf222
gnnchya <75212443+gnnchya@users.noreply.github.com>
2021-06-16 09:39:34 +0700

    Update interface.go

--- superHero/newApp2/service/user/interface.go
+++ superHero/newApp2/service/user/interface.go
@@ -3,16 +3,12 @@ package user
 import (
 	"context"
 
-	"github.com/gnnchya/InternAtTouch/tree/Develop-optimized/newApp/domain"
 	"github.com/gnnchya/InternAtTouch/tree/Develop-optimized/newApp/service/user/userin"
 )
 
 type Service interface {
 	Update(ctx context.Context, input *userin.UpdateInput) (ID string, err error)
 	Create(ctx context.Context, input *userin.CreateInput) (ID string, err error)
-	Search(ctx context.Context, input *userin.Search) (ID string, err error)
 	Delete(ctx context.Context, input *userin.DeleteInput) (ID string, err error)
-	View(ctx context.Context, input *userin.ViewInput) (a domain.InsertQ, err error)
-	ViewAll(ctx context.Context, input *userin.ViewAllInput) (a []domain.InsertQ, err error)
 	MsgReceiver(ctx context.Context, msg []byte) (err error)
 }
