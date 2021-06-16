532f61bc5892f91768334e14bbdc57b97993ae95
proudkittapa <proudy2013@gmail.com>
2021-06-16 10:42:48 +0700

    -

--- superHero/newApp/service/msgbroker/implement/private.go
+++ superHero/newApp/service/msgbroker/implement/private.go
@@ -1,17 +1,24 @@
 package implement
 
 import (
+	"context"
 	"fmt"
 	"github.com/gnnchya/InternAtTouch/tree/Develop-optimized/newApp/service/msgbroker/msgbrokerin"
 	"github.com/touchtechnologies-product/message-broker/common"
-	"context"
 )
 
 func (impl implementation) newHandler(topic msgbrokerin.TopicMsgBroker) (handler common.Handler) {
 	return func(ctx context.Context, msg []byte) {
 		var err error
 		switch topic {
-		case msgbrokerin.TopicUser:
+		case msgbrokerin.TopicResponseCreate:
+			fmt.Println("enter newHandler responseCreate topic")
+			err = impl.usrService.MsgReceiver(ctx, msg)
+		case msgbrokerin.TopicResponseUpdate:
+			fmt.Println("enter newHandler responseUpdate topic")
+			err = impl.usrService.MsgReceiver(ctx, msg)
+		case msgbrokerin.TopicResponseDelete:
+			fmt.Println("enter newHandler responseDel topic")
 			err = impl.usrService.MsgReceiver(ctx, msg)
 		default:
 			fmt.Println(string(msg), "with default")
--- superHero/newApp/service/user/userin/msgbroker.go
+++ superHero/newApp/service/user/userin/msgbroker.go
@@ -5,13 +5,19 @@ import "github.com/gnnchya/InternAtTouch/tree/Develop-optimized/newApp/service/m
 type MsgBrokerCreate struct{
 	Action msgbrokerin.ActionMsgBroker `json:"action"`
 	ID         string   `json:"id"`
-	Name       string   `json:"name" validate:"required"`
+	Name       string   `son:"name" validate:"required"`
 	ActualName string   `json:"actual_name" validate:"required"`
+	ActualLastName string `json:"actual_last_name"`
 	Gender     string   `json:"gender"`
 	BirthDate  int64    `json:"birth_date"`
 	Height     int      `json:"height" validate:"gte=0"`
 	SuperPower []string `json:"super_power"`
 	Alive      bool     `json:"alive"`
+	Universe       string   `json:"universe"`
+	Movies         []string `json:"movies"`
+	Enemies        []string `json:"enemies"`
+	FamilyMember   []string `json:"family_member"`
+	About          string   `json:"about"`
 }
 
 func (msg MsgBrokerCreate) ToCreateInput()(createInput *CreateInput){
@@ -19,12 +25,67 @@ func (msg MsgBrokerCreate) ToCreateInput()(createInput *CreateInput){
 		ID:         msg.ID,
 		Name:       msg.Name,
 		ActualName: msg.ActualName,
+		ActualLastName: msg.ActualLastName,
 		Gender:     msg.Gender,
 		BirthDate:  msg.BirthDate,
 		Height:     msg.Height,
 		SuperPower: msg.SuperPower,
 		Alive:      msg.Alive,
+		Universe: msg.Universe,
+		Movies: msg.Movies,
+		Enemies: msg.Enemies,
+		FamilyMember: msg.FamilyMember,
+		About: msg.About,
 	}
 	return createInput
 }
 
+type MsgBrokerUpdate struct {
+	Action msgbrokerin.ActionMsgBroker `json:"action"`
+	ID         string   `json:"id"`
+	Name       string   `son:"name" validate:"required"`
+	ActualName string   `json:"actual_name" validate:"required"`
+	ActualLastName string `json:"actual_last_name"`
+	Gender     string   `json:"gender"`
+	BirthDate  int64    `json:"birth_date"`
+	Height     int      `json:"height" validate:"gte=0"`
+	SuperPower []string `json:"super_power"`
+	Alive      bool     `json:"alive"`
+	Universe       string   `json:"universe"`
+	Movies         []string `json:"movies"`
+	Enemies        []string `json:"enemies"`
+	FamilyMember   []string `json:"family_member"`
+	About          string   `json:"about"`
+}
+
+func (msg MsgBrokerUpdate) ToUpdateInput()(input *UpdateInput) {
+	input = &UpdateInput{
+		ID:         msg.ID,
+		Name:       msg.Name,
+		ActualName: msg.ActualName,
+		ActualLastName: msg.ActualLastName,
+		Gender:     msg.Gender,
+		BirthDate:  msg.BirthDate,
+		Height:     msg.Height,
+		SuperPower: msg.SuperPower,
+		Alive:      msg.Alive,
+		Universe: msg.Universe,
+		Movies: msg.Movies,
+		Enemies: msg.Enemies,
+		FamilyMember: msg.FamilyMember,
+		About: msg.About,
+	}
+	return input
+}
+
+type MsgBrokerDelete struct {
+	Action msgbrokerin.ActionMsgBroker `json:"action"`
+	ID string `json:"id"`
+}
+
+func (msg MsgBrokerDelete) ToUpdateInput()(input *DeleteInput) {
+	input = &DeleteInput{
+		ID: msg.ID,
+	}
+	return input
+}
