4ff4876e115fc2deb310aceffccda6cd108b761b
pondnarawich <pondnarawich@gmail.com>
2021-06-17 10:36:56 +0700

    -

--- elasticSearch/search.go
+++ elasticSearch/search.go
@@ -8,7 +8,11 @@ import (
 	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/elastic/go-elasticsearch/v8/esapi"
 	"log"
-	"reflect"
+	"strconv"
+
+	//"math/rand"
+	//"strconv"
+
 	//"strconv"
 
 	//"strconv"
@@ -16,7 +20,38 @@ import (
 	//"sync"
 )
 
-//var r  map[string]interface{}
+var r map[string]interface{}
+type sp struct {
+	Index			string		`json:"_index"`
+	Type			string		`json:"_type"`
+	Id				string		`json:"_id"`
+	Score			float32		`json:"_score"`
+	Source 			InsertStruct
+}
+
+type prelast struct{
+	fuck []string
+	final last
+}
+
+type last struct{
+	took	int		`json:"name"`
+	time_out bool	`json:"time_out"`
+	_shard struct{
+		total int `json:"total"`
+		successful int `json:"successful"`
+		skipped int `json:"skipped"`
+		failed int `json:"failed"`
+	} `json:"_shard"`
+	hits struct{
+		total struct {
+			value int `json:"value"`
+			relation string `json:"relation"`
+		}
+		max_score float32`json:"max_score"`
+		hits []sp `json:"hits"`
+	}`json:"hits"`
+}
 
 
 //func buildRequest(keyword string) bytes.Buffer {
@@ -108,17 +143,19 @@ func search(ctx context.Context,es *elasticsearch.Client, res *esapi.Response, b
 			)
 		}
 	}
-	s := res.String()
-a := strings.Split(s,"hits")
-fmt.Println(a[2])
 
-	//json.Unmarshal([]byte(s), &data)
-	//fmt.Println(data)
-	//fmt.Println(res.String())
+	//s := res.String()
+//a := strings.Split(s,"hits")
+////fmt.Println(s)
+//	var s struct{}
+	//var data map[string]interface{}
+	//json.Unmarshal([]byte(res.String()), &s)
 	if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
 		log.Fatalf("Error parsing the response body: %s", err)
 	}
-
+	//json.Marshal(data)
+	//fmt.Println("asd",s)
+	//fmt.Println(fmt.Sprintf("%v", data))
 }
 
 //func add(ctx context.Context,es *elasticsearch.Client, res *esapi.Response, buf bytes.Buffer, err error){
@@ -144,11 +181,9 @@ func main() {
 	ctx := context.Background()
 
 	// Create a mapping for the Elasticsearch documents
-	var (
-		docMap map[string]interface{}
-	)
-	fmt.Println("docMap:", docMap)
-	fmt.Println("docMap TYPE:", reflect.TypeOf(docMap))
+	//var docMap map[string]interface{}
+	//fmt.Println("docMap:", docMap)
+	//fmt.Println("docMap TYPE:", reflect.TypeOf(docMap))
 
 	// Declare an Elasticsearch configuration
 	cfg := elasticsearch.Config{
@@ -187,44 +222,23 @@ func main() {
 	//)
 	// Print the ID and document source for each hit.
 	//
-	//var temp Sp
-	//var ans []Sp
-	//for _, hit := range r["hits"].(map[string]interface{})["hits"].([]interface{}) {
-	//
-	//	s := hit.(map[string]interface{})["_source"]
-	//	log.Println("type :",reflect.TypeOf(s.(map[string]interface{})["birth_date"]))
-	//	xxx := s.(map[string]interface{})["birth_date"])
-	//	temp.Name = fmt.Sprintf("%v", s.(map[string]interface{})["name"])
-	//	temp.ActualName = fmt.Sprintf("%v", s.(map[string]interface{})["actual_name"])
-	//	temp.ActualLastName = fmt.Sprintf("%v", s.(map[string]interface{})["actual_lastname"])
-	//	temp.Gender = fmt.Sprintf("%v", s.(map[string]interface{})["gender"])
-	//	temp.BirthDate = int64(xxx)
-	//	temp.Height,_ = strconv.Atoi(fmt.Sprintf("%v", s.(map[string]interface{})["height"]))
-	//	temp.SuperPower = strings.Split(fmt.Sprintf("%v", s.(map[string]interface{})["super_power"]),",")
-	//	temp.Alive,_ = strconv.ParseBool(fmt.Sprintf("%v", s.(map[string]interface{})["alive"]))
-	//	temp.Universe = fmt.Sprintf("%v", s.(map[string]interface{})["universe"])
-	//	temp.Movies = strings.Split(fmt.Sprintf("%v", s.(map[string]interface{})["movies"]),",")
-	//	temp.Enemies = strings.Split(fmt.Sprintf("%v", s.(map[string]interface{})["enemies"]),",")
-	//	temp.FamilyMember = strings.Split(fmt.Sprintf("%v", s.(map[string]interface{})["family_member"]),",")
-	//	temp.About = fmt.Sprintf("%v", s.(map[string]interface{})["about"])
-	//	//x := (s.(map[string]interface{})["birth_date"]).(int64)
-	//	//fmt.Println(x)
-	//	ans = append(ans, temp)
-	//	}
-	//	for _, s := range ans{
-	//		fmt.Println(s)
-	//	}
-	//}
-	//for _, hit := range r["hits"].(map[string]interface{})["hits"].([]interface{}) {
-	//
-	//	for _, s := range hit.(map[string]interface{})["_source"].(map[string]interface{}) {
-	//		str := fmt.Sprintf("%v", s)
-	//		fmt.Println("pond",str)
-	//	}
-	//
-	//
-	//
-	//}
-	fmt.Println(r)
-
+	var temp InsertStruct
+	var ans []InsertStruct
+	for _, hit := range r["hits"].(map[string]interface{})["hits"].([]interface{}) {
+		s := hit.(map[string]interface{})["_source"]
+		temp.Name = fmt.Sprintf("%v", s.(map[string]interface{})["name"])
+		temp.ActualName = fmt.Sprintf("%v", s.(map[string]interface{})["actual_name"])
+		temp.ActualLastName = fmt.Sprintf("%v", s.(map[string]interface{})["actual_lastname"])
+		temp.Gender = fmt.Sprintf("%v", s.(map[string]interface{})["gender"])
+		temp.BirthDate = int64(s.(map[string]interface{})["birth_date"].(float64))
+		temp.Height,_ = strconv.Atoi(fmt.Sprintf("%v", s.(map[string]interface{})["height"]))
+		temp.SuperPower = strings.Split(fmt.Sprintf("%v", s.(map[string]interface{})["super_power"]),",")
+		temp.Alive,_ = strconv.ParseBool(fmt.Sprintf("%v", s.(map[string]interface{})["alive"]))
+		temp.Universe = fmt.Sprintf("%v", s.(map[string]interface{})["universe"])
+		temp.Movies = strings.Split(fmt.Sprintf("%v", s.(map[string]interface{})["movies"]),",")
+		temp.Enemies = strings.Split(fmt.Sprintf("%v", s.(map[string]interface{})["enemies"]),",")
+		temp.FamilyMember = strings.Split(fmt.Sprintf("%v", s.(map[string]interface{})["family_member"]),",")
+		temp.About = fmt.Sprintf("%v", s.(map[string]interface{})["about"])
+		ans = append(ans, temp)
+		}
 }
--- superHero/newApp/domain/query.go
+++ superHero/newApp/domain/query.go
@@ -54,3 +54,21 @@ type ViewByPageQ struct {
 	PerPage int
 	Page    int
 }
+
+type InsertStruct struct {
+	Name      		string   `bson:"name" json:"name" validate:"required"`
+	ActualName 		string   `bson:"actual_name" json:"actual_name"`
+	ActualLastName  string   `bson:"actual_lastname" json:"actual_lastname"`
+	Gender     		string   `bson:"gender" json:"gender"`
+	BirthDate  		int64    `bson:"birth_date" json:"birth_date"`
+	Height     		int      `bson:"height" json:"height" validate:"gte=0"`
+	SuperPower 		[]string `bson:"super_power" json:"super_power"`
+	Alive      		bool     `bson:"alive" json:"alive"`
+	Universe 		string	 `bson:"universe" json:"universe"`
+	Movies			[]string `bson:"movies" json:"movies"`
+	Enemies			[]string `bson:"enemies" json:"enemies"`
+	FamilyMember	[]string `bson:"family_member" json:"family_member"`
+	About			string	 `bson:"about" json:"about"`
+	Code int `json:"code"`
+	Err string `json:"err"`
+}
